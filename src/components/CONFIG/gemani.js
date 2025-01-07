33e3eecc87cc77ab94a673b1405db888085a85ea
ankit <ankitjangid6375@gmail.com>
2025-01-07 12:29:43 -0500

    remove api key

--- src/components/CONFIG/gemani.js
+++ src/components/CONFIG/gemani.js
@@ -6,7 +6,7 @@ import {
     HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyCwOgZseLsODimtSEW2jiiZEKiZYBvL4Qg";
+const apiKey = "😎⬇⬇ Enter Your Api key";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
